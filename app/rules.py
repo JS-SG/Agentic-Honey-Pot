b4685b604ec177accbe9d06e1011edb5404392a3
JS-SG <144525322+JS-SG@users.noreply.github.com>
2026-02-16 06:03:04 +0530

    Update rules.py

--- app/rules.py
+++ app/rules.py
@@ -4,9 +4,7 @@ UPI_REGEX = r"\b[a-zA-Z0-9.\-_]{2,}@[a-zA-Z0-9.\-]{2,}\b"
 BANK_REGEX = r"\b(?:account|acc|a\/c|ac)(?:\s*(?:no|number|#))?\s*(?:is|:|-|\()?(\d{9,18})\)?\b"
 GENERIC_ACCOUNT_REGEX = r"\b\d{12,18}\b"
 IFSC_REGEX = r"\b[A-Z]{4}0[A-Z0-9]{6}\b"
-"""URL_REGEX = r"(https?://[^\s]+|www\.[^\s]+)"""
 URL_REGEX = r"(https?://[^\s]+|www\.[^\s]+|\b[a-zA-Z0-9.-]+\.(com|in|net|org|co|info)(/[^\s]*)?)"
-"""PHONE_REGEX = r"\+91[\-\s]?\d{10}|\b[6-9]\d{9}\b"""
 PHONE_REGEX = r"\b(?:\+91[\s-]?|0)?[6-9]\d{9}\b|\b1[0-9]{3}[-\s]?[0-9]{3}[-\s]?[0-9]{4}\b"
 email_regex = r"\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b"
 KEYWORDS = ["urgent", "verify", "blocked", "suspended", "account", "transfer",  "inactive", "immediately",
@@ -48,6 +46,7 @@ def analyze_message(message: str):
         "emailAddresses": emails,
         "keywords": keywords_found
     }
+
 
 
 
